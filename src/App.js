bafb40cd650322dd9328617b817dc461c8e8d3b8
nikolainiklaus <118966948+nikolainiklaus@users.noreply.github.com>
2023-02-20 16:40:09 +0100

    Revert "Revert "navbar-right""
    This reverts commit ee540ec2295d99bd9bce84cbd7be6b8428b89758.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import "./styles/RightNavbar.css";
+
+import RightNavbar from "./components/RightNavbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <RightNavbar />
+    </div>
+  );
 }
 
 export default App;
