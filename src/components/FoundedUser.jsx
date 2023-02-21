7f251cfb45b7c72a48e8e63b33abb6b6f062ff61
Muhammed Yasir Özdemir <yasiryksmail@gmail.com>
2023-02-21 21:56:27 +0300

    right modal styling done

--- src/components/FoundedUser.jsx
+++ src/components/FoundedUser.jsx
@@ -10,7 +10,7 @@ const FoundedUsers = ({ user }) => {
   return (
     <>
       <Link onClick={hideTab} to={"/in/" + user._id}>
-        <Row className="align-items-center mb-2">
+        <Row className="align-items-center my-2">
           <Col xs={2}>
             <div
               className="d-flex justify-content-center"
