56ed932600519cf38f0544d87f9c2ab9fc4c4e9d
Muhammed Yasir Özdemir <yasiryksmail@gmail.com>
2023-02-24 08:30:43 +0300

    like & dislike works :maple_leaf:

--- src/components/SinglePost.jsx
+++ src/components/SinglePost.jsx
@@ -3,7 +3,7 @@ import { Col, Row, Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 // import { editPost } from "../redux/actions";
-import { doEditPost, LIKE, likePost } from "../redux/actions";
+import { dislikePost, doEditPost, LIKE, likePost } from "../redux/actions";
 import { useParams } from "react-router-dom";
 import { removePost } from "../redux/actions";
 import "../styles/SinglePost.css";
@@ -12,8 +12,7 @@ const SinglePost = ({ post }) => {
   const dispatch = useDispatch();
 
   const likedPosts = useSelector((state) => state.likedPosts);
-  const isLiked = likedPosts.some((p) => post?.id === p._id);
-  console.log(isLiked);
+  const isLiked = likedPosts.some((p) => post?._id === p._id);
 
   const myInfo = useSelector((state) => state.me);
   const [wannaEdit, setWannaEdit] = useState(false);
@@ -41,7 +40,6 @@ const SinglePost = ({ post }) => {
 
   const params = useParams();
   const postId = params.postId;
-  console.log(post?._id);
 
   return (
     <Row className="postRow">
@@ -135,19 +133,33 @@ const SinglePost = ({ post }) => {
           </Form>
         )}
         <Row className="postBtnWrapper">
-          <button
-            // dispatch(likePost(post));
-            onClick={() => {
-              {
-                isLiked ? console.log("a") : console.log("b");
-              }
-            }}
-          >
-            <svg viewBox="0 0 24 24" width="24" height="24">
-              <path d="M19.46 11l-3.91-3.91a7 7 0 01-1.69-2.74l-.49-1.47A2.76 2.76 0 0010.76 1 2.75 2.75 0 008 3.74v1.12a9.19 9.19 0 00.46 2.85L8.89 9H4.12A2.12 2.12 0 002 11.12a2.16 2.16 0 00.92 1.76A2.11 2.11 0 002 14.62a2.14 2.14 0 001.28 2 2 2 0 00-.28 1 2.12 2.12 0 002 2.12v.14A2.12 2.12 0 007.12 22h7.49a8.08 8.08 0 003.58-.84l.31-.16H21V11zM19 19h-1l-.73.37a6.14 6.14 0 01-2.69.63H7.72a1 1 0 01-1-.72l-.25-.87-.85-.41A1 1 0 015 17l.17-1-.76-.74A1 1 0 014.27 14l.66-1.09-.73-1.1a.49.49 0 01.08-.7.48.48 0 01.34-.11h7.05l-1.31-3.92A7 7 0 0110 4.86V3.75a.77.77 0 01.75-.75.75.75 0 01.71.51L12 5a9 9 0 002.13 3.5l4.5 4.5H19z"></path>
-            </svg>
-            <span className="ml-1">Like</span>
-          </button>
+          {post && isLiked ? (
+            <>
+              <button
+                onClick={() => {
+                  dispatch(dislikePost(post?._id));
+                }}
+                style={{ color: "#0A66C2" }}
+              >
+                <img
+                  src="https://static.licdn.com/sc/h/5zhd32fqi5pxwzsz78iui643e"
+                  alt=""
+                ></img>
+                <span className="ml-1">Like</span>
+              </button>
+            </>
+          ) : (
+            <button
+              onClick={() => {
+                dispatch(likePost(post));
+              }}
+            >
+              <svg viewBox="0 0 24 24" width="24" height="24">
+                <path d="M19.46 11l-3.91-3.91a7 7 0 01-1.69-2.74l-.49-1.47A2.76 2.76 0 0010.76 1 2.75 2.75 0 008 3.74v1.12a9.19 9.19 0 00.46 2.85L8.89 9H4.12A2.12 2.12 0 002 11.12a2.16 2.16 0 00.92 1.76A2.11 2.11 0 002 14.62a2.14 2.14 0 001.28 2 2 2 0 00-.28 1 2.12 2.12 0 002 2.12v.14A2.12 2.12 0 007.12 22h7.49a8.08 8.08 0 003.58-.84l.31-.16H21V11zM19 19h-1l-.73.37a6.14 6.14 0 01-2.69.63H7.72a1 1 0 01-1-.72l-.25-.87-.85-.41A1 1 0 015 17l.17-1-.76-.74A1 1 0 014.27 14l.66-1.09-.73-1.1a.49.49 0 01.08-.7.48.48 0 01.34-.11h7.05l-1.31-3.92A7 7 0 0110 4.86V3.75a.77.77 0 01.75-.75.75.75 0 01.71.51L12 5a9 9 0 002.13 3.5l4.5 4.5H19z"></path>
+              </svg>
+              <span className="ml-1">Like</span>
+            </button>
+          )}
           <button>
             <svg viewBox="0 0 24 24" width="24" height="24">
               <path d="M7 9h10v1H7zm0 4h7v-1H7zm16-2a6.78 6.78 0 01-2.84 5.61L12 22v-4H8A7 7 0 018 4h8a7 7 0 017 7zm-2 0a5 5 0 00-5-5H8a5 5 0 000 10h6v2.28L19 15a4.79 4.79 0 002-4z"></path>
