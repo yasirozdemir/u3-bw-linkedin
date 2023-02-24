c492b0309ba52d2a1d46e073b5c8d2034348ce08
Muhammed Yasir Özdemir <yasiryksmail@gmail.com>
2023-02-24 09:08:54 +0300

    post modal :ballot_box_with_check:

--- src/components/PostInput.jsx
+++ src/components/PostInput.jsx
@@ -1,17 +1,15 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { Form } from "react-bootstrap";
 import { MdOutlineSaveAlt } from "react-icons/md";
 import { useDispatch, useSelector } from "react-redux";
 import { addPost } from "../redux/actions";
 import "../styles/PostInput.css";
+import PostModal from "./PostModal";
 
 function PostInput() {
   const dispatch = useDispatch();
-  const inputRef = useRef(null);
 
-  const imitateInputFile = () => {
-    inputRef.current.click();
-  };
+  const [showPostModal, setShowPostModal] = useState(false);
 
   const myInfo = useSelector((state) => state.me);
   const [post, setPost] = useState({
@@ -47,48 +45,34 @@ function PostInput() {
   };
 
   return (
-    <div id="createPostWrapper">
-      <Form className="post-form" onSubmit={handleSubmit}>
-        <div className="d-flex w-100 mt-2">
+    <>
+      <PostModal
+        post={post}
+        handleInputChange={handleInputChange}
+        handleSubmit={handleSubmit}
+        handleFileChange={handleFileChange}
+        myInfo={myInfo}
+        showPostModal={showPostModal}
+        setShowPostModal={setShowPostModal}
+      />
+      <div id="createPostWrapper">
+        <div className="d-flex w-100">
           <div
             className="d-flex align-items-center justify-content-center mr-2"
             style={{ width: "48px", height: "48px", objectFit: "cover" }}
           >
             <img src={myInfo?.image} alt="" className="w-100 rounded-circle" />
           </div>
-          <Form.Group style={{ flexGrow: "0.95" }}>
-            <Form.Control
-              type="text"
-              name="text"
-              value={post.text}
-              placeholder="Start a post..."
-              onChange={handleInputChange}
-            />
-          </Form.Group>
-        </div>
-        <div className="d-flex align-items-center justify-content-center">
-          <button type="submit" className="mr-3">
-            <MdOutlineSaveAlt
-              className="mr-1"
-              style={{ fontSize: "1.5rem", transform: "rotate(180deg)" }}
-            />
-            Share
+          <button
+            onClick={() => {
+              setShowPostModal(true);
+            }}
+          >
+            Start a post
           </button>
-          <button onClick={imitateInputFile}>
-            <svg className="mr-1" viewBox="0 0 24 24" width="24" height="24">
-              <path d="M16 13a4 4 0 11-4-4 4 4 0 014 4zm6-4v11H2V9a3 3 0 013-3h1.3l1.2-3h9l1.2 3H19a3 3 0 013 3zm-5 4a5 5 0 10-5 5 5 5 0 005-5z"></path>
-            </svg>
-            Add photo
-          </button>
-          <Form.File
-            className="d-none"
-            ref={inputRef}
-            id="imageFile"
-            onChange={handleFileChange}
-          />
         </div>
-      </Form>
-    </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/PostModal.jsx
@@ -0,0 +1,90 @@
+import { useRef } from "react";
+import { Form } from "react-bootstrap";
+import Modal from "react-bootstrap/Modal";
+import { MdOutlineSaveAlt } from "react-icons/md";
+
+const PostModal = ({
+  showPostModal,
+  setShowPostModal,
+  myInfo,
+  post,
+  handleInputChange,
+  handleSubmit,
+  handleFileChange,
+}) => {
+  const inputRef = useRef(null);
+  const imitateInputFile = () => {
+    inputRef.current.click();
+  };
+
+  return (
+    <>
+      <Modal
+        id="postModal"
+        show={showPostModal}
+        onHide={() => {
+          setShowPostModal(false);
+        }}
+      >
+        <Modal.Header closeButton>
+          <h5 className="m-0">Create a post</h5>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="d-flex align-items-center w-100">
+            <div
+              className="d-flex align-items-center justify-content-center mr-2"
+              style={{ width: "48px", height: "48px", objectFit: "cover" }}
+            >
+              <img
+                src={myInfo?.image}
+                alt=""
+                className="w-100 rounded-circle"
+              />
+            </div>
+            <h6 style={{ fontWeight: "600" }}>
+              {myInfo?.name} {myInfo?.surname}
+            </h6>
+          </div>
+          <Form onSubmit={handleSubmit}>
+            <Form.Control
+              id="post-text-input"
+              as="textarea"
+              name="text"
+              value={post.text}
+              placeholder="What do you want to talk about?"
+              onChange={handleInputChange}
+            />
+            <div className="d-flex align-items-center justify-content-center mt-auto">
+              <button type="submit" className="mr-3">
+                <MdOutlineSaveAlt
+                  className="mr-1"
+                  style={{ fontSize: "1.5rem", transform: "rotate(180deg)" }}
+                />
+                Share
+              </button>
+              <button onClick={imitateInputFile}>
+                <svg
+                  className="mr-1"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  height="24"
+                >
+                  <path d="M16 13a4 4 0 11-4-4 4 4 0 014 4zm6-4v11H2V9a3 3 0 013-3h1.3l1.2-3h9l1.2 3H19a3 3 0 013 3zm-5 4a5 5 0 10-5 5 5 5 0 005-5z"></path>
+                </svg>
+                Add photo
+              </button>
+              <Form.File
+                className="d-none"
+                ref={inputRef}
+                id="imageFile"
+                onChange={handleFileChange}
+              />
+            </div>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+};
+
+export default PostModal;
